6568616f9826e8adada1abac6e17100151495243
Kirill Apasov <kirill.apasov@yandex.ru>
2024-03-11 14:32:01 +0300

    Add realisation for calculateHolidaysInVacation(Employee employee, int specifiedVacationDays) & calculateWeekendsInVacation. Create two boolean helper method (isHoliday & isWeekend)

--- src/main/java/com/example/calc/service/CalcService.java
+++ src/main/java/com/example/calc/service/CalcService.java
@@ -1,6 +1,13 @@
 package com.example.calc.service;
 import com.example.calc.model.Employee;
 import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class CalcService {
         public double calculateVacationCompensation(Employee employee) {
@@ -18,12 +25,42 @@ public class CalcService {
 
         private int calculateHolidaysInVacation(Employee employee, int specifiedVacationDays) {
             int holidays = 0;
+            LocalDate startDate = LocalDate.now();
+            LocalDate endDate = startDate.plusDays(specifiedVacationDays);
+            while (!startDate.isAfter(endDate)) {
+                if (isHoliday(startDate)) {
+                    holidays++;
+                }
+                startDate = startDate.plusDays(1);
+            }
             return holidays;
+        }
+        private boolean isHoliday(LocalDate date) {
+            List<LocalDate> holidays = Arrays.asList(
+                    LocalDate.of(date.getYear(), Month.JANUARY, 1),
+                    LocalDate.of(date.getYear(), Month.JANUARY, 7),
+                    LocalDate.of(date.getYear(), Month.MAY, 1),
+                    LocalDate.of(date.getYear(), Month.MAY, 9),
+                    LocalDate.of(date.getYear(), Month.JUNE, 12),
+                    LocalDate.of(date.getYear(), Month.NOVEMBER, 4)
+            );
+            return holidays.contains(date);
         }
 
 
         private int calculateWeekendsInVacation(Employee employee, int specifiedVacationDays) {
-            int weekends = 0;
-            return weekends;
+                int weekends = 0;
+                LocalDate startDate = LocalDate.now();
+                LocalDate endDate = startDate.plusDays(specifiedVacationDays);
+                while (!startDate.isAfter(endDate)) {
+                    if (isWeekend(startDate)) {
+                        weekends++;
+                    }
+                    startDate = startDate.plusDays(1);
+                }
+                return weekends;
+            }
+        private boolean isWeekend(LocalDate date) {
+            return date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY;
         }
 }
